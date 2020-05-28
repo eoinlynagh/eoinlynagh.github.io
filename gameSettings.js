6bf315aa4c06ff959296c9f2b431118f08d735bb
Eoin <eoinlynaghgithub@gmail.com>
2020-05-27 22:39:36 -0400

    Update gameSettings.js
    added some new puzzles

--- gameSettings.js
+++ gameSettings.js
@@ -5,6 +5,8 @@ function ButtonSwap(idSelector, stage) {
     switch (stage) {
         case 0:
         case 1:
+        case 2:
+        case 3:
             var matrix = createMatrix()
             var position = getPosition(idSelector);
             var IDs = getFourIDs(position, matrix);
@@ -12,8 +14,8 @@ function ButtonSwap(idSelector, stage) {
                 alternater(item)
             });
             break;
-        case 2:
-        case 3:
+        case 4:
+        case 5:
             var matrix = createMatrix()
             var position = getPosition(idSelector);
             var IDs = getFourIDs(position, matrix);
@@ -37,15 +39,21 @@ function ButtonSwap(idSelector, stage) {
 function getSet(stage) {
     switch (stage) {
         case 0:
-            var set = ['a1', 'a2', 'a3', 'a4', 'b1', 'b4', 'c1', 'c4', 'd1', 'd2', 'd3', 'd4']
+            var set = ['a2', 'a3', 'b1', 'b4', 'c1', 'c4', 'd2', 'd3']
             break;
         case 1:
-            var set = ['a1', 'a2', 'a3', 'a4', 'b1', 'b2', 'b3', 'b4', 'c1', 'c2', 'c3', 'c4', 'd1', 'd2', 'd3', 'd4']
+            var set = ['a1', 'a2', 'a3', 'a4', 'b1', 'b4', 'c1', 'c4', 'd1', 'd2', 'd3', 'd4']
             break;
         case 2:
-            var set = ['a1', 'a2', 'b1', 'b2', 'c3', 'c4', 'd3', 'd4']
+            var set = ['a1', 'a2', 'a3', 'a4', 'b1', 'b2', 'b3', 'b4', 'c1', 'c2', 'c3', 'c4', 'd1', 'd2', 'd3', 'd4']
             break;
         case 3:
+                var set = ['a2', 'a3', 'b2', 'b3', 'c2', 'c3', 'd2', 'd3']
+                break;
+        case 4:
+            var set = ['a1', 'a2', 'b1', 'b2', 'c3', 'c4', 'd3', 'd4']
+            break;
+        case 5:
             var set = ['a1', 'a3', 'b2', 'b4', 'c1', 'c3', 'd2', 'd4']
             break;
         default:
@@ -57,8 +65,8 @@ function getSet(stage) {
 
 function getResetSet(stage){
     switch (stage) {
-        case 2:
-        case 3:
+        case 4:
+        case 5:
             set = ['a4', 'b3']; //set of buttons to be red intially
             break;
         default:
